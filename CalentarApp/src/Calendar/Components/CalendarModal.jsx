90748a98f67f78d6f1cf5dce6edef3f42e982067
martinchoprograma2023 <martincarbonelli1981@gmail.com>
2023-09-05 10:15:52 -0300

    nuevos cambios

--- CalentarApp/src/Calendar/Components/CalendarModal.jsx
+++ CalentarApp/src/Calendar/Components/CalendarModal.jsx
@@ -1,5 +1,14 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import Modal from "react-modal";
+import "../../styles.css";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+import { addHours, differenceInSeconds } from "date-fns";
+import DatePicker, { registerLocale } from "react-datepicker";
+import es from "date-fns/locale/es";
+registerLocale("es", es);
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const customStyles = {
   content: {
@@ -12,13 +21,58 @@ const customStyles = {
   },
 };
 
-Modal.setAppElement("#root");
+Modal.setAppElement();
 
 export const CalendarModal = () => {
   const [isOpen, setIsOpen] = useState(true);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const [formValues, setFormValues] = useState({
+    title: "marcos",
+    notes: "gallardi",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+  });
+  
+//useMemo para que cuando el formValues.title cambie y el formSubmited tambien cambie el estilo del imput hay error
+  const titleClass = useMemo(() => {
+    if (!formSubmitted) return "";
+    return formValues.title.length > 0 ? "is-valid" : "is-invalid";
+  }, [formValues.title, formSubmitted]);
 
   const onCloseModal = () => {
     setIsOpen(false);
+  };
+
+  const onInputChange = ({ target }) => {
+    setFormValues({ ...formValues, [target.name]: target.value });
+  };
+
+  const onDateChange = (event, chaning) => {
+    setFormValues({ ...formValues, [chaning]: event });
+  };
+
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+
+    //forma de saber que el form se intento postear
+    setFormSubmitted(true);
+    //forma de saber que el form se intento postear
+
+    //forma de ver la diferencia de horario entre inicio y fin de evento
+    const difference = differenceInSeconds(formValues.end, formValues.start);
+    //forma de ver la diferencia de horario entre inicio y fin de evento
+    console.log({ difference });
+    if (isNaN(difference) || difference <= 0) {
+      Swal.fire("Fechas incorrectas", "Revisar fechas ingresadas", "error");
+
+      return;
+    }
+
+    if (formValues.title.length <= 0) {
+      alert("Falta titulo");
+      return;
+    }
   };
 
   return (
@@ -27,15 +81,78 @@ export const CalendarModal = () => {
       onRequestClose={onCloseModal}
       style={customStyles}
       className="modal"
-      overlayClassName="modal-fondo"
-      closeTimeoutMS={200}>
-      <h2>Hola mundo</h2>
+      overlayClassName="modal-fondo">
+      <h1> Nuevo evento </h1>
       <hr />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-        provident necessitatibus possimus ullam fuga ab libero placeat, expedita
-        nam minima?
-      </p>
+      <form className="container" onSubmit={onSubmitForm}>
+        <div className="m-4">
+          <div className="form-group mb-2">
+            <label>Fecha y hora inicio</label>
+            <br />
+            <DatePicker
+              selected={formValues.start}
+              onChange={(event) => onDateChange(event, "start")}
+              dateFormat="Pp"
+              className="form-control"
+              showTimeSelect
+              locale="es"
+              timeCaption="Hora"
+            />
+          </div>
+
+          <div className="form-group mb-2">
+            <label>Fecha y hora fin</label>
+            <br />
+            <DatePicker
+              selected={formValues.end}
+              onChange={(event) => onDateChange(event, "end")}
+              minDate={formValues.start}
+              dateFormat="Pp"
+              className="form-control"
+              showTimeSelect
+              locale="es"
+              timeCaption="Hora"
+            />
+          </div>
+
+          <hr />
+          <div className="form-group mb-2">
+            <label>Titulo y notas</label>
+            <input
+              type="text"
+              className={`form-control ${titleClass}`}
+              placeholder="Título del evento"
+              name="title"
+              value={formValues.title}
+              onChange={onInputChange}
+              autoComplete="off"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              Una descripción corta
+            </small>
+          </div>
+
+          <div className="form-group mb-2">
+            <textarea
+              type="text"
+              className="form-control"
+              placeholder="Notas"
+              rows="5"
+              F
+              name="notes"
+              value={formValues.notes}
+              onChange={onInputChange}></textarea>
+            <small id="emailHelp" className="form-text text-muted">
+              Información adicional
+            </small>
+          </div>
+
+          <button type="submit" className="btn btn-outline-primary btn-block">
+            <i className="far fa-save"></i>
+            <span> Guardar</span>
+          </button>
+        </div>
+      </form>
     </Modal>
   );
 };

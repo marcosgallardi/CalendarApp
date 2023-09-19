fc92327df4520328ed120943e27bec040f604285
marcosgallardi <gallardimarcos98@gmail.com>
2023-09-19 15:02:15 -0300

    actualizacion por error con otra cuenta de git

--- CalendarSv/index.js
+++ CalendarSv/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const server = express()
 require("dotenv").config()
-console.log(process.env)
+
 
 server.listen(process.env.PORT,()=>{
     console.log(`Server listen in port ${process.env.PORT}`)

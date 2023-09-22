bd2fd093856c2c31dcb9cbdf53bb0ed8a3ea73c8
marcosgallardi <gallardimarcos98@gmail.com>
2023-09-22 16:38:29 -0300

    cambios

--- CalendarSv/controllers/auth.js
+++ CalendarSv/controllers/auth.js
@@ -1,14 +1,9 @@
+const { validationResult } = require("express-validator");
+
 const crearUsuario = (req, res) => {
   const { name, email, password } = req.body;
 
-  if (name.length < 5) {
-    return res.status(400).json({
-      ok: false,
-      msg: "El nombre debe tener mas de 5 caracteres",
-    });
-  }
-
-  res.json({
+  res.status(201).json({
     ok: true,
     name,
     email,
@@ -17,6 +12,8 @@ const crearUsuario = (req, res) => {
 };
 
 const loginUsuario = (req, res) => {
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
     msg: "login",
--- CalendarSv/routes/auth.js
+++ CalendarSv/routes/auth.js
@@ -1,14 +1,40 @@
 const { Router } = require("express");
 const router = Router();
+const { check } = require("express-validator");
 const {
   crearUsuario,
   revalidarToken,
   loginUsuario,
 } = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validar-campos");
 
-router.post("/new", crearUsuario);
+router.post(
+  "/new",
 
-router.post("/", loginUsuario);
+  [
+    //middleware
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+    validarCampos
+  ],
+  crearUsuario
+);
+
+router.post(
+  "/",
+  [
+    //middleware
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+    validarCampos
+  ],
+  loginUsuario
+);
 
 router.get("/renew", revalidarToken);
 
